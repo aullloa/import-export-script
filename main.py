e1bb233c871700efb96ae67453e48fa01007e9f0
Alfredo Ulloa <alfredo.ulloa.58@my.csun.edu>
2025-09-27 09:54:29 -0700

    script complete

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ xytech_lines = xytech_file.readlines()
 for b_line in baselight_lines:
     eachLine = b_line.strip().split()
     url = eachLine[0].strip("/baselightfilesystem1")
-    print(url)
     for x_line in xytech_lines:
         if not (x_line.startswith("/")):
             continue
@@ -21,9 +20,7 @@ for b_line in baselight_lines:
             if (x_line.__contains__(url)):
                 new_url = x_line
                 break
-    print(new_url)
     frames = list(map(int, eachLine[1:])) # Converting to int to check sequence
-    print(frames)
 
     starting_frame = frames[0]
     ending_frame = frames[0]
@@ -37,4 +34,11 @@ for b_line in baselight_lines:
                 row = [new_url, f"{starting_frame}-{ending_frame}"]
             writer.writerow(row)
             starting_frame = ending_frame = frame #reset trackers
+
+    # This handles logic when the last frame on the list is reached
+    if starting_frame == ending_frame:
+        row = [new_url, starting_frame]
+    else:
+        row = [new_url, f"{starting_frame}-{ending_frame}"]
+    writer.writerow(row)
 
