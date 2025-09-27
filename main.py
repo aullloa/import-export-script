34d115489b50bdfda9caf46c9446bec48a27603d
Alfredo Ulloa <alfredo.ulloa.58@my.csun.edu>
2025-09-26 23:44:06 -0700

    csv data added; logic for frames reqd

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-
+import csv
 
 baselight_file = open("Baselight_export_fall2025.txt", "r")
-xytech_file = open("Xytech_fall2025.txt", "r+")
+xytech_file = open("Xytech_fall2025.txt", "r")
+nerd_file = open("Nerd_export_fall2025.csv", "w")
+writer = csv.writer(nerd_file)
 
-for line in baselight_file:
-    eachLine = line.strip().split()
-    url =  eachLine[0].strip("/baselightfilesystem1")
-    base_frames = {
-        "url": url,
-        "frames": eachLine[1:]
-    }
-    print(base_frames)
+baselight_lines = baselight_file.readlines()
+xytech_lines = xytech_file.readlines()
 
+for b_line in baselight_lines:
+    eachLine = b_line.strip().split()
+    url = eachLine[0].strip("/baselightfilesystem1")
+    print(url)
+    for x_line in xytech_lines:
+        if not (x_line.startswith("/")):
+            continue
+        else:
+            if (x_line.__contains__(url)):
+                new_url = x_line
+                break
+    print(new_url)
+    writer.writerow([new_url, ", ".join(eachLine[1:])])
 
-for line in xytech_file:
-    if not(line.startswith("/")):
-        continue
-    else:
-        eachLine = line.strip().split()
-        url = eachLine[0]
-        print(url)
